c5a8fbaa761fc49cba5e84512ec1576d07ebe0c3
Ady G <60425995+AdyGCode@users.noreply.github.com>
2020-08-24 12:00:12 +0800

    Created Blinky, plus support code (#6)

---
+++ ghost_blinky.py
@@ -0,0 +1,52 @@
+"""
+    Ghost: Blinky
+
+    This file defines the frames for Blinky
+
+"""
+
+from sensible_colours import *
+
+blinky_frames = [
+    [
+        k, k, r, r, r, rd, k, k,
+        k, r, r, r, r, r, rd, k,
+        r, r, w, k, r, w, k, rd,
+        r, r, w, w, r, w, w, rd,
+        r, r, r, r, r, r, rd, rd,
+        r, r, r, r, r, r, rd, rd,
+        r, r, r, r, r, r, rd, rd,
+        r, k, r, r, k, r, rd, rd
+    ],
+    [
+        k, k, r, r, r, rd, k, k,
+        k, r, r, r, r, r, rd, k,
+        r, r, w, k, r, w, k, rd,
+        r, r, w, w, r, w, w, rd,
+        r, r, r, r, r, r, rd, rd,
+        r, r, r, r, r, r, rd, rd,
+        r, r, r, r, r, r, rd, rd,
+        r, k, r, r, r, k, rd, rd
+    ],
+    [
+        k, k, r, r, r, rd, k, k,
+        k, r, r, r, r, r, rd, k,
+        r, r, k, w, r, k, w, rd,
+        r, r, w, w, r, w, w, rd,
+        r, r, r, r, r, r, rd, rd,
+        r, r, r, r, r, r, rd, rd,
+        r, r, r, r, r, r, rd, rd,
+        r, r, k, r, r, r, k, rd
+    ],
+    [
+        k, k, r, r, r, rd, k, k,
+        k, r, r, r, r, r, rd, k,
+        r, r, k, w, r, k, w, rd,
+        r, r, w, w, r, w, w, rd,
+        r, r, r, r, r, r, rd, rd,
+        r, r, r, r, r, r, rd, rd,
+        r, r, r, r, r, r, rd, rd,
+        r, r, k, r, r, k, rd, rd
+    ],
+
+]
