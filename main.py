6f021fe2ecba467212caa407f65c4fe50b8cf88f
Ady G <60425995+AdyGCode@users.noreply.github.com>
2020-08-24 11:43:34 +0800

    Created Blinky, plus support code

---
+++ main.py
@@ -0,0 +1,30 @@
+from sense_hat import SenseHat
+import time
+import sensible_colours
+from ghost_blinky import blinky_frames
+
+
+def fill_pixels(the_sense_hat, the_animation):
+    the_sense_hat.low_light = True
+
+    num_frames = len(the_animation)
+    count = 0
+    while True:
+        pixels = the_animation[count % num_frames]
+        the_sense_hat.set_pixels(pixels)
+        time.sleep(1 / 4)
+        count += 1
+
+
+if __name__ == '__main__':
+    # Initialise the SenseHAT and clear the LED matrix
+    sense = SenseHat()
+    sense.clear([0, 0, 0])
+    sense.low_light = True
+
+    frames = []
+
+    for frame in blinky_frames:
+        frames.append(frame)
+
+    fill_pixels(sense, frames)
---
+++ sensible_colours.py
@@ -0,0 +1,45 @@
+"""
+SenseHAT Colours
+
+These colours provide reasonable colour variation to be visible when
+displayed on the SenseHAT.
+
+The values of Red, Green and Blue that provide good variations are:
+
+    0, 48, 96, 144, 192, 240, 255
+
+"""
+
+red = r = [255, 0, 0]
+orange = o = [255, 144, 0]
+yellow = y = [255, 255, 0]
+lime = l = [128, 255, 0]
+green = g = [0, 255, 0]
+turquoise = t = [0, 255, 128]
+cyan = c = [0, 255, 255]
+aqua = a = [0, 128, 255]
+blue = b = [0, 0, 255]
+purple = p = [128, 0, 255]
+magenta = m = [255, 0, 255]
+pink = i = [255, 0, 128]
+
+red_dark = rd = [128, 0, 0]
+orange_dark = od = [128, 64, 0]
+yellow_dark = yd = [128, 128, 0]
+lime_dark = ld = [64, 128, 0]
+green_dark = gd = [0, 128, 0]
+turquoise_dark = td = [0, 128, 64]
+cyan_dark = cd = [0, 128, 128]
+aqua_dark = ad = [0, 64, 128]
+blue_dark = bd = [0, 0, 128]
+purple_dark = pd = [64, 0, 128]
+magenta_dark = md = [128, 0, 128]
+pink_dark = id = [128, 0, 64]
+
+white = w = [255, 255, 255]
+grey_bright = eb = [240, 240, 240]
+grey_light = el = [192, 192, 192]
+grey = e = [144, 144, 144]
+grey_dark = ed = [96, 96, 96]
+charcoal = h = [48, 48, 48]
+black = k = [0, 0, 0]
